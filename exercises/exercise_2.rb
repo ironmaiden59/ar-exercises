70067ba0aaedc7c28700cb7c30011c3ab3ab72d5
ralph <ralphdcouto1987@gmail.com>
2023-10-19 23:58:25 -0400

    Exercise 2: Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Load the first store and assign it to @store1
+@store1 = Store.find(1)
+
+# Load the second store and assign it to @store2
+@store2 = Store.find(2)
+
+# Update the first store
+@store1.update(name: 'New Burnaby')
