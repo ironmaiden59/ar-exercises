f353c3d2a37bc6d74041f3b65ed27e2ead6b07e1
ralph <ralphdcouto1987@gmail.com>
2023-10-20 00:13:43 -0400

    Exercise 7: Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,36 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :carry_apparel
+
+  def carry_apparel
+    errors.add(:base, "Stores must carry at least one of the men's or women's apparel") unless mens_apparel || womens_apparel
+  end
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+end
+
+# Ask the user for a store name
+print "Enter a store name: "
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name
+new_store = Store.create(name: store_name)
+
+# Display the error messages provided back from ActiveRecord
+if new_store.errors.any?
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+else
+  puts "Store created successfully!"
+end
