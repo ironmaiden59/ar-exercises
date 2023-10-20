8c146c551978e3a8ea42e22c406462fdcc032c7d
ralph <ralphdcouto1987@gmail.com>
2023-10-20 00:02:48 -0400

    Exercise 4: Loading a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Create 3 more stores
+Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Fetch stores that carry men's apparel
+@mens_stores = Store.where(mens_apparel: true)
+
+# Output name and annual revenue of men's apparel stores
+@mens_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
+
+# Fetch stores that carry women's apparel and have an annual revenue of less than $1M
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: 0...1000000)
